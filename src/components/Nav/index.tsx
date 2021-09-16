6bc5c28e75295b5a1a9b904f7d519edfac71d984
Žiga Franko <franko.ziga@gmail.com>
2021-09-16 18:10:08 +0200

    Dropdown component and Nav WIP

---
+++ src/components/Nav/index.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+interface NavProps {
+
+}
+
+export const HorizontalNav: React.FC<{}> = ({children}): JSX.Element => (
+  <nav>
+    {children}
+  </nav>
+);
+
+export const VerticalNav: React.FC<{}> = ({children}) => (
+  <nav>
+    {children}
+  </nav>
+)
+
+export const HorizontalContent: React.FC<{}> = ({children}) => (
+  <div className="d-flex justify-content-center">
+    <div className="d-flex w-auto nav-selection border-rad">
+      {children}
+    </div>
+  </div>
+)
--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -2,11 +2,47 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { BackIcon, DownArrowIcon, PlusIcon } from '../Icons';
 
+// TODO move this out
 export interface ButtonStatus {
   text: string;
   isValid: boolean;
 }
 
+interface NavProps {
+  to: string;
+  selected: boolean;
+}
+
+interface ButtonProps {
+  onClick?: () => void;
+}
+
+export const Button: React.FC<ButtonProps> = ({children, onClick=() => {}}): JSX.Element => (
+  <button type="button" className="btn btn-reef" onClick={onClick}>
+    {children}
+  </button>
+)
+
+export const Empty: React.FC<ButtonProps> = ({children, onClick=() => {}}): JSX.Element => (
+  <button type="button" className="btn" onClick={onClick}>
+    {children}
+  </button>
+)
+
+export const Nav: React.FC<NavProps> = ({ to, children, selected = false }): JSX.Element => (
+  <Link to={to} className={`border-rad h-100 fs-6 fw-bold px-3 py-2 ${selected ? 'nav-selected' : 'nav-button'}`}>
+    {children}
+  </Link>
+);
+
+// TODO maybe we do not need this one?
+export const Back = ({ onClick }: ButtonProps): JSX.Element => (
+  <Empty onClick={onClick}>
+    <BackIcon />
+  </Empty>
+);
+
+// TODO Move out or refactore!
 interface SwitchTokenButton {
   addIcon?: boolean;
   disabled?: boolean;
@@ -15,12 +51,6 @@ interface SwitchTokenButton {
 
 interface IconButton {
   onClick?: () => void;
-}
-
-interface NavButtonProps {
-  to: string;
-  name: string;
-  selected: boolean;
 }
 
 export const SwitchToken = ({ addIcon, disabled, onClick }: SwitchTokenButton): JSX.Element => (
@@ -36,19 +66,5 @@ export const SwitchToken = ({ addIcon, disabled, onClick }: SwitchTokenButton): 
 export const Icon: React.FC<IconButton> = ({ onClick, children }): JSX.Element => (
   <button type="button" className="btn btn-select border-rad px-2 py-1" onClick={onClick} data-bs-dismiss="modal">
     {children}
-  </button>
-);
-
-export const Nav = ({ to, name, selected = false }: NavButtonProps): JSX.Element => (
-  <Link to={to} className={`border-rad h-100 fs-6 fw-bold px-3 py-2 ${selected ? 'nav-selected' : 'nav-button'}`}>{name}</Link>
-);
-
-interface Back {
-  onBack: () => void;
-}
-
-export const Back = ({ onBack }: Back): JSX.Element => (
-  <button type="button" className="btn" onClick={onBack}>
-    <BackIcon />
   </button>
 );
