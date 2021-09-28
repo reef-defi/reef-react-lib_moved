b0ff7b45f79c1db83aee974bcafa1c83a9621330
Žiga Franko <franko.ziga@gmail.com>
2021-09-29 00:01:57 +0200

    buttons renamed

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -14,7 +14,7 @@ export const Button: React.FC<ButtonProps> = ({
   </button>
 );
 
-export const Empty: React.FC<ButtonProps> = ({
+export const EmptyButton: React.FC<ButtonProps> = ({
   children,
   onClick = () => {},
 }): JSX.Element => (
@@ -24,10 +24,10 @@ export const Empty: React.FC<ButtonProps> = ({
 );
 
 // TODO maybe we do not need this one?
-export const Back = ({ onClick }: ButtonProps): JSX.Element => (
-  <Empty onClick={onClick}>
+export const BackButton = ({ onClick }: ButtonProps): JSX.Element => (
+  <EmptyButton onClick={onClick}>
     <BackIcon />
-  </Empty>
+  </EmptyButton>
 );
 
 // TODO Move out or refactore!
@@ -60,7 +60,7 @@ export const SwitchToken = ({
   </div>
 );
 
-export const Icon: React.FC<IconButton> = ({
+export const IconButton: React.FC<IconButton> = ({
   onClick,
   children,
 }): JSX.Element => (
