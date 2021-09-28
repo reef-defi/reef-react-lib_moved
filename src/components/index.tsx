dd3b05582a8ae2450955ffb28483e07572427d5f
Žiga Franko <franko.ziga@gmail.com>
2021-09-28 15:52:54 +0200

    navigation removed

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -7,4 +7,3 @@ export * as Input from './common/Input';
 export * as Label from './common/Label';
 export * as Loading from './common/Loading';
 export * as Modal from './common/Modal';
-export * as Nav from './common/Nav';
