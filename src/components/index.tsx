9e2b6fa41c46739b38677f549ecddf34060a3865
Žiga Franko <franko.ziga@gmail.com>
2021-09-30 19:32:17 +0200

    exports update

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,10 +1,3 @@
-export * as Alert from './common/Alert';
-export * as Button from './common/Button';
-export * as Card from './common/Card';
-export * as Dropdown from './common/Dropdown';
-export * as Icons from './common/Icons';
-export * as Input from './common/Input';
-export * as Label from './common/Label';
-export * as Text from './common/Text';
-export * as Loading from './common/Loading';
-export * as Modal from './common/Modal';
+export * from "./common";
+export * from "./SelectToken";
+export * from "./TokenFields";
