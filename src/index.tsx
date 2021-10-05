179ab37c470e3a0aecb4f320108994aa85e7158e
Matjaz Hirsman <matjazonline@gmail.com>
2021-10-05 14:07:27 +0200

    provider initialization support (#5)
    * support app initialization
    
    * fix code review
    
    * compile fix
    
    * compile fix
    
    * expand interface
    
    * resolve pr #5

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,5 @@ export * from './state';
 export * as rpc from './rpc';
 export * as hooks from './hooks';
 export * as utils from './utils';
+export * as api from './api';
 export * as Components from './components';
