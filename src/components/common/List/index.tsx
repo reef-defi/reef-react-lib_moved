47606935026d093d7adbdd0179e77354990e05e9
Žiga Franko <franko.ziga@gmail.com>
2021-10-08 12:19:04 +0200

    additional display components, list item now closes modal and can accept onClick method

--- src/components/common/List/index.tsx
+++ src/components/common/List/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
+interface ListItem {
+  onClick?: () => void;
+}
+
 export const List: React.FC<unknown> = ({ children }): JSX.Element => (
   <ul className="list-group list-group-flush list-group-full px-0">
     {children}
   </ul>
 );
 
-export const ListItem: React.FC<unknown> = ({ children }): JSX.Element => (
-  <li className="list-group-item list-group-item-action d-flex justify-content-between">
+export const ListItem: React.FC<ListItem> = ({ children, onClick=() => {} }): JSX.Element => (
+  <li
+    className="list-group-item list-group-item-action d-flex justify-content-between"
+    onClick={onClick}
+    data-bs-dismiss="modal"
+  >
     {children}
   </li>
 );
