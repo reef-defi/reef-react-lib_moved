57deacdbe99b7952d53ce6b39e9598092c6249ec
Matjaz Hirsman <matjazonline@gmail.com>
2021-12-21 00:37:48 +0100

    expose bind and transfer native

--- src/utils/bindUtil.ts
+++ src/utils/bindUtil.ts
@@ -1,25 +1,38 @@
 import { Provider } from '@reef-defi/evm-provider';
 import { ReefSigner } from '../state';
+import { TxStatusHandler } from './transactionUtil';
 
-export const alertEvmAddressBind = async (signer: ReefSigner, provider: Provider): Promise<void> => {
+export const alertEvmAddressBind = async (signer: ReefSigner, provider: Provider, onTxChange?: TxStatusHandler): Promise<void> => {
   if (!provider) {
     return Promise.resolve();
   }
   if (signer && !signer?.isEvmClaimed) {
+    const txIdent = Math.random().toString(10);
     // eslint-disable-next-line no-restricted-globals,no-alert
     const isDefault = confirm('Create default Ethereum VM address for this account on Reef chain.');
     if (isDefault) {
       try {
         await signer.signer.claimDefaultAccount();
       } catch (err) {
+        let message = '';
         if (err && (typeof err === 'string') && err.startsWith('1010')) {
-          alert('Add some Reef coins\nto this account\nand try again.');
+          message = 'Add some Reef coins\nto this account\nand try again.';
         } else {
-          alert(`Transaction failed, err= ${err.toString()}`);
+          message = `Transaction failed, err= ${err.toString()}`;
         }
+        if (onTxChange) {
+          onTxChange({ txIdent, error: message });
+        } else {
+          alert(message);
+        }
+
         return Promise.resolve();
       }
-      alert(`Created Ethereum VM address is ${signer.evmAddress}.\nNow you're ready to use full functionality of Reef chain.`);
+      if (!onTxChange) {
+        alert(`Created Ethereum VM address is ${signer.evmAddress}.\nNow you're ready to use full functionality of Reef chain.`);
+      } else {
+        onTxChange({ txIdent, isInBlock: true });
+      }
     } else {
       // TODO return claimEvmAccount(currentSigner, provider);
     }
--- src/utils/transactionUtil.ts
+++ src/utils/transactionUtil.ts
@@ -1,3 +1,8 @@
+import { Provider } from '@reef-defi/evm-provider';
+import { BigNumber } from 'ethers';
+import { handleTxResponse } from '@reef-defi/evm-provider/utils';
+import { ReefSigner } from '../state';
+
 export const TX_TYPE_EVM = 'TX_TYPE_EVM';
 
 export type TxStatusHandler = (status: TxStatusUpdate)=>void;
@@ -11,3 +16,43 @@ export interface TxStatusUpdate {
   type?: string;
   url?: string;
 }
+
+export const handleErr = (e: {message: string}|string, txIdent:string, txHash: string, txHandler: TxStatusHandler): void => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  let reason = e.message || e;
+  if (reason && (reason.indexOf('-32603: execution revert: 0x') > -1 || reason?.indexOf('InsufficientBalance') > -1)) {
+    reason = 'You must allow minimum 60 REEF on account for Ethereum VM transaction even if transaction fees will be much lower.';
+  }
+  if (reason && (reason?.startsWith('1010'))) {
+    reason = 'Balance too low.';
+  }
+  txHandler({
+    txIdent, txHash, error: reason,
+  });
+};
+
+export const sendToNativeAddress = async (provider: Provider, signer: ReefSigner, toAmt: BigNumber, to: string, txHandler: TxStatusHandler): Promise<string> => {
+  const transfer = provider.api.tx.balances.transfer(to, toAmt.toString());
+  const substrateAddress = await signer.signer.getSubstrateAddress();
+  const txIdent = Math.random().toString(10);
+  transfer.signAndSend(substrateAddress, { signer: signer.signer.signingKey },
+    (res) => handleTxResponse(res, provider.api).then(
+      (txRes: any): void => {
+        const txHash = transfer.hash.toHex();
+        txHandler({
+          txIdent, txHash, isInBlock: txRes.result.status.isInBlock, isComplete: txRes.result.status.isFinalized,
+        });
+      },
+    ).catch((rej: any) => {
+      // finalized error is ignored
+      if (rej.result.status.isInBlock) {
+        const txHash = transfer.hash.toHex();
+        handleErr(rej.message, txIdent, txHash, txHandler);
+      }
+    })).catch((e) => {
+    console.log('sendToNativeAddress err=', e);
+    handleErr(e, txIdent, '', txHandler);
+  });
+  return Promise.resolve(txIdent);
+};
