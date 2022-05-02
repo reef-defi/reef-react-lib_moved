006b2e534747d655996d4fa0be3ea9d052cb0738
Matjaz Hirsman <matjazonline@gmail.com>
2022-05-02 14:17:21 +0200

    Display NFTs in actions (#52)

--- src/utils/transactionUtil.ts
+++ src/utils/transactionUtil.ts
@@ -1,6 +1,6 @@
 import { Provider } from '@reef-defi/evm-provider';
 import { BigNumber } from 'ethers';
-import { ReefSigner } from '../state';
+import { availableNetworks, Network, ReefSigner } from '../state';
 
 export type TxStatusHandler = (status: TxStatusUpdate) => void;
 
@@ -99,3 +99,5 @@ export const sendToNativeAddress = (
 
   return txIdent;
 };
+
+export const getExtrinsicUrl = (hash: string, network: Network = availableNetworks.mainnet): string => `${network.reefscanFrontendUrl}/extrinsic/${hash}`;
