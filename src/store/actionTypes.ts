8f4e888ed99eb951c7554b55d64bd958e41656b5
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-05-11 19:14:40 +0200

    default reducers and actions for swap

---
+++ src/store/actionTypes.ts
@@ -0,0 +1,14 @@
+// Swap action type names
+export const SET_POOL = 'SET_POOL';
+export const SET_FOCUS = 'SET_FOCUS';
+export const SET_TOKEN1 = 'SET_TOKEN1';
+export const SET_TOKEN2 = 'SET_TOKEN2';
+export const SET_SETTINGS = 'SET_SETTINGS';
+export const SWITCH_TOKENS = 'SWITCH_TOKENS';
+export const SET_TOKEN1_AMOUNT = 'SET_TOKEN1_AMOUNT';
+export const SET_TOKEN2_AMOUNT = 'SET_TOKEN2_AMOUNT';
+
+export const SET_STATUS = 'SET_STATUS';
+export const SET_COMPLETE_STATUS = 'SET_COMPLETE_STATUS';
+export const SET_LOADING = 'SET_LOADING';
+export const SET_VALIDITY = 'SET_VALIDITY';
