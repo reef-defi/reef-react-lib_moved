93791626d08b1763a43916c35411f37d62e97fb0
Matjaz Hirsman <matjazonline@gmail.com>
2022-06-17 11:56:25 +0200

    save selected network

--- src/appState/providerState.ts
+++ src/appState/providerState.ts
@@ -5,13 +5,14 @@ import { Network } from '../state';
 const providerSubj: ReplaySubject<Provider> = new ReplaySubject<Provider>(1);
 const selectedNetworkSubj: ReplaySubject<Network> = new ReplaySubject<Network>();
 
+export const ACTIVE_NETWORK_LS_KEY = 'reef-app-active-network';
 export const currentProvider$ = providerSubj.asObservable().pipe(shareReplay(1));
 export const setCurrentProvider = (provider: Provider): void => providerSubj.next(provider);
 
 export const currentNetwork$ = selectedNetworkSubj.asObservable();
 export const setCurrentNetwork = (network: Network): void => {
   if (network != null) {
-    localStorage.setItem('reef-app-active-network', JSON.stringify(network));
+    localStorage.setItem(ACTIVE_NETWORK_LS_KEY, JSON.stringify(network));
   }
   selectedNetworkSubj.next(network);
 };
--- src/hooks/useInitReefState.ts
+++ src/hooks/useInitReefState.ts
@@ -2,45 +2,22 @@ import { useEffect, useState } from 'react';
 import { useObservableState } from './useObservableState';
 import { availableNetworks, Network } from '../state';
 import { useProvider } from './useProvider';
-import { currentNetwork$, setCurrentNetwork, setCurrentProvider, } from '../appState/providerState';
+import {
+  ACTIVE_NETWORK_LS_KEY,
+  currentNetwork$,
+  setCurrentNetwork,
+  setCurrentProvider,
+} from '../appState/providerState';
 import { accountsSubj } from '../appState/accountState';
 import { useLoadSigners } from './useLoadSigners';
 import { disconnectProvider } from '../utils/providerUtil';
 import { initApolloClient, State, StateOptions } from '../appState/util';
 
-const ACTIVE_NETWORK_KEY = 'reef-app-active-network';
-/*
-const getGQLUrls = (network: Network): { ws: string; http: string }|undefined => {
-  if (!network.graphqlUrl) {
-    return undefined;
-  }
-  const ws = network.graphqlUrl.startsWith('http')
-    ? network.graphqlUrl.replace('http', 'ws')
-    : network.graphqlUrl;
-  const http = network.graphqlUrl.startsWith('ws')
-    ? network.graphqlUrl.replace('ws', 'http')
-    : network.graphqlUrl;
-  return { ws, http };
-};
-
 const getNetworkFallback = (): Network => {
-  const storedNetwork = localStorage.getItem(ACTIVE_NETWORK_KEY);
+  const storedNetwork = localStorage.getItem(ACTIVE_NETWORK_LS_KEY);
   return storedNetwork != null ? JSON.parse(storedNetwork) : availableNetworks.mainnet;
 };
 
-interface State {
-  loading: boolean;
-  signers?: ReefSigner[];
-  provider?: Provider;
-  network?: Network;
-  error?: any; // TODO!
-}
-
-interface StateOptions {
-  network?: Network;
-  signers?: ReefSigner[];
-  client?: ApolloClient<any>;
-}*/
 export const useInitReefState = (
   applicationDisplayName: string,
   options: StateOptions = {},
