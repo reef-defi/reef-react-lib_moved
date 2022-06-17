359ba555342b455325ef6327ef1809cefd9dd5ed
Metod Ribič <metodribic@users.noreply.github.com>
2022-06-17 12:25:52 +0200

    feat: persist active network to localStorage (#86)
    * feat: persist active network to localStorage
    
    `network` in `StateOptions` is now optional and should not be passed to `useInitReefState` unless in rare occasions since the default network is now loaded from localStorage. If active network is not present in localStorage hook (`useInitReefState`) then defaults to mainnet.
    
    * Init state without react (#87)
    
    * save selected network
    
    Co-authored-by: Matjaz Hirsman <matjazonline@gmail.com>

--- src/appState/providerState.ts
+++ src/appState/providerState.ts
@@ -5,9 +5,15 @@ import { Network } from '../state';
 const providerSubj: ReplaySubject<Provider> = new ReplaySubject<Provider>(1);
 const selectedNetworkSubj: ReplaySubject<Network> = new ReplaySubject<Network>();
 
+export const ACTIVE_NETWORK_LS_KEY = 'reef-app-active-network';
 export const currentProvider$ = providerSubj.asObservable().pipe(shareReplay(1));
 export const setCurrentProvider = (provider: Provider): void => providerSubj.next(provider);
 
-export const currentNetwork$ = selectedNetworkSubj.asObservable()
-export const setCurrentNetwork = (network: Network): void => selectedNetworkSubj.next(network);
+export const currentNetwork$ = selectedNetworkSubj.asObservable();
+export const setCurrentNetwork = (network: Network): void => {
+  if (network != null) {
+    localStorage.setItem(ACTIVE_NETWORK_LS_KEY, JSON.stringify(network));
+  }
+  selectedNetworkSubj.next(network);
+};
 currentNetwork$.subscribe((network) => console.log('SELECTED NETWORK=', network.rpcUrl));
