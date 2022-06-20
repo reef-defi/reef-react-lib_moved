a8fb6ab92578759fd581886eed230d305a2d7bd4
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-06-21 01:46:30 +0200

    pool stats simplified

--- src/hooks/usePoolStats.ts
+++ src/hooks/usePoolStats.ts
@@ -1,60 +1,47 @@
-import { ApolloClient } from "@apollo/client";
-import { BigNumber } from "ethers";
-import { useState } from "react";
-import { Pool24HVolume, PoolsTotalSupply, POOLS_TOTAL_SUPPLY, PoolVolume24HVar, POOL_24H_VOLUME } from "../graphql/pools";
-import { getTokenPrice, Pool, TokenPrices } from "../state";
-import { useAsyncEffect } from "./useAsyncEffect";
+import { useQuery } from "@apollo/client";
+import { BigNumber } from "bignumber.js";
+import { useMemo } from "react";
+import { Pool24HVolume, PoolsTotalSupply, POOLS_TOTAL_VALUE_LOCKED, PoolVolume24HVar, POOL_24H_VOLUME } from "../graphql/pools";
+import { getTokenPrice, TokenPrices } from "../state";
 
-export const useTotalSupply = (tokenPrices: TokenPrices, client: ApolloClient<any>): string => {
-  const [totalSupply, setTotalSupply] = useState("0");
+export const useTotalSupply = (tokenPrices: TokenPrices): string => {
+  const {data} = useQuery<PoolsTotalSupply>(POOLS_TOTAL_VALUE_LOCKED);
 
-  // Updating token supply
-  useAsyncEffect(async () => {
-    const supplies = await client.query<PoolsTotalSupply>({
-        query: POOLS_TOTAL_SUPPLY,
-      })
-      .then((res) => res.data.pool_event);
+  if (!data || data.pool_event.length === 0) {
+    return "0";
+  }
 
-    const supply = supplies
-      .reduce((acc, {pool: {token_1, token_2}, reserved_1, reserved_2}) => {
-        const tokenPrice1 = getTokenPrice(token_1, tokenPrices)
-        const tokenPrice2 = getTokenPrice(token_2, tokenPrices)
-
-      return acc
-        .add(tokenPrice1.mul(reserved_1))
-        .add(tokenPrice2.mul(reserved_2));
-    }, BigNumber.from(0));
-
-    setTotalSupply(supply.toString());
-  }, [tokenPrices])
-
-  return totalSupply
+  return data.pool_event.reduce((acc, {reserved_1, reserved_2, pool: {token_1, token_2}}) => {
+    const tokenPrice1 = getTokenPrice(token_1, tokenPrices)
+    const tokenPrice2 = getTokenPrice(token_2, tokenPrices)
+    const r1 = tokenPrice1.multipliedBy(new BigNumber(reserved_1).div(new BigNumber(10).pow(18)));
+    const r2 = tokenPrice2.multipliedBy(new BigNumber(reserved_2).div(new BigNumber(10).pow(18)));
+    return acc.plus(r1).plus(r2);
+  }, new BigNumber(0)).toString();
 }
 
-export const usePoolVolume = (pools: Pool[], tokenPrices: TokenPrices, client: ApolloClient<any>): string => {
-  const [volume, setVolume] = useState("0");
-
-  useAsyncEffect(async () => {
-    const volumes = await client.query<Pool24HVolume, PoolVolume24HVar>({
-        query: POOL_24H_VOLUME,
-        variables: {
-          fromTime: new Date(Date.now() - 24*60*60*1000).toUTCString()
-        }
-      })
-      .then((res) => res.data.pool_hour_volume);
-
-    const summedVolumes = volumes.reduce((acc, {amount_1, amount_2, pool: {token_1, token_2}}) => {
-      const tokenPrice1 = getTokenPrice(token_1, tokenPrices);
+export const usePoolVolume = (tokenPrices: TokenPrices): string => {
+  const fromTime = useMemo(
+    () => new Date(Date.now() - 24*60*60*1000).toISOString(),
+    []
+  );
+  const {data} = useQuery<Pool24HVolume, PoolVolume24HVar>(
+    POOL_24H_VOLUME,
+    {
+      variables: { fromTime }
+    }
+  );
+  if (!data || data.pool_hour_volume.length === 0) {
+    return "0";
+  }
+  
+  return data.pool_hour_volume.reduce((acc, {amount_1, amount_2, pool: {token_1, token_2}}) => {
+    const tokenPrice1 = getTokenPrice(token_1, tokenPrices);
       const tokenPrice2 = getTokenPrice(token_2, tokenPrices);
 
-      return acc
-        .add(tokenPrice1.mul(amount_1))
-        .add(tokenPrice2.mul(amount_2));
-    }, BigNumber.from(0))
-
-    setVolume(summedVolumes.toString());
-  }, [pools, tokenPrices]);
-
-  return volume;
+    return acc
+      .plus(tokenPrice1.multipliedBy(new BigNumber(amount_1).div(new BigNumber(10).pow(18))))
+      .plus(tokenPrice2.multipliedBy(new BigNumber(amount_2).div(new BigNumber(10).pow(18))));
+  }, new BigNumber(0)).toString();
 };
 
