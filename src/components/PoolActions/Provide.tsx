5fa55cd2e37a3c83500190095ad52d847fe63bb1
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-07-27 18:19:19 +0200

    working provide liquidity - without confirmation modal!
    Confirmation modal has a css bug that needs to be resolved

--- src/components/PoolActions/Provide.tsx
+++ src/components/PoolActions/Provide.tsx
@@ -1,16 +1,14 @@
-import React, { useMemo, useState } from 'react';
+import { faCoins } from '@fortawesome/free-solid-svg-icons';
 import Uik from '@reef-defi/ui-kit';
 import BigNumber from 'bignumber.js';
-import { faCoins } from '@fortawesome/free-solid-svg-icons';
-import {
-  resolveSettings, TokenWithAmount, Pool,
-} from '../../state';
+import React, { useMemo, useState } from 'react';
+// import { Pool, TokenWithAmount } from '../../state';
 import { AddLiquidityState } from '../../store';
-import { calculatePoolShare } from '../../utils/math';
+// import { calculatePoolShare } from '../../utils/math';
 import TokenField from './TokenField';
 
-import { ConfirmLabel } from '../common/Label';
-import ConfirmationModal from '../common/Modal';
+// import { ConfirmLabel } from '../common/Label';
+// import ConfirmationModal from '../common/Modal';
 
 export interface ProvideActions {
   onAddLiquidity: () => Promise<void>,
@@ -23,88 +21,89 @@ export interface Props {
   actions: ProvideActions
 }
 
-const Modal = ({
-  onAddLiquidity,
-  newPoolSupply,
-  token1,
-  token2,
-  percentage,
-  pool,
-}: {
-  onAddLiquidity: () => Promise<void>,
-  newPoolSupply: string,
-  token1: TokenWithAmount,
-  token2: TokenWithAmount,
-  percentage: number,
-  pool?: Pool
-}): JSX.Element => (
-  <ConfirmationModal
-    id="supplyModalToggle"
-    title="Confirm Supply"
-    confirmFun={onAddLiquidity}
-  >
-    <label className="text-muted ms-2">You will recieve</label>
+// TODO Samo fix modal
+// const Modal = ({
+//   onAddLiquidity,
+//   newPoolSupply,
+//   token1,
+//   token2,
+//   percentage,
+//   pool,
+// }: {
+//   onAddLiquidity: () => Promise<void>,
+//   newPoolSupply: string,
+//   token1: TokenWithAmount,
+//   token2: TokenWithAmount,
+//   percentage: number,
+//   pool?: Pool
+// }): JSX.Element => (
+//   <ConfirmationModal
+//     id="supplyModalToggle"
+//     title="Confirm Supply"
+//     confirmFun={onAddLiquidity}
+//   >
+//     <label className="text-muted ms-2">You will recieve</label>
 
-    <div className="field border-rad p-3">
-      <ConfirmLabel
-        titleSize="h4"
-        valueSize="h6"
-        title={newPoolSupply}
-        value={`${token1.name}/${token2.name}`}
-      />
-    </div>
-    <div className="m-3">
-      <span className="mini-text text-muted d-inline-block">
-        Output is estimated. If the price changes by more than
-        {' '}
-        {percentage}
-        % your transaction will revert.
-      </span>
-    </div>
-    <div className="field p-2 border-rad">
-      <ConfirmLabel
-        title="Liquidity Provider Fee"
-        value="1.5 REEF"
-        titleSize="mini-text"
-        valueSize="mini-text"
-      />
-      <ConfirmLabel
-        title={`${token1.name} Deposited`}
-        value={`${token1.amount}`}
-        titleSize="mini-text"
-        valueSize="mini-text"
-      />
-      <ConfirmLabel
-        title={`${token2.name} Deposited`}
-        value={`${token2.amount}`}
-        titleSize="mini-text"
-        valueSize="mini-text"
-      />
-      <ConfirmLabel
-        title="Rates"
-        value={`1 ${token1.name} = ${(
-          token1.price / token2.price
-        ).toFixed(8)} ${token2.name}`}
-        titleSize="mini-text"
-        valueSize="mini-text"
-      />
-      <ConfirmLabel
-        title=""
-        value={`1 ${token2.name} = ${(
-          token2.price / token1.price
-        ).toFixed(8)} ${token1.name}`}
-        titleSize="mini-text"
-        valueSize="mini-text"
-      />
-      <ConfirmLabel
-        title="Share of Pool"
-        value={`${calculatePoolShare(pool).toFixed(8)} %`}
-        titleSize="mini-text"
-        valueSize="mini-text"
-      />
-    </div>
-  </ConfirmationModal>
-);
+//     <div className="field border-rad p-3">
+//       <ConfirmLabel
+//         titleSize="h4"
+//         valueSize="h6"
+//         title={newPoolSupply}
+//         value={`${token1.name}/${token2.name}`}
+//       />
+//     </div>
+//     <div className="m-3">
+//       <span className="mini-text text-muted d-inline-block">
+//         Output is estimated. If the price changes by more than
+//         {' '}
+//         {percentage}
+//         % your transaction will revert.
+//       </span>
+//     </div>
+//     <div className="field p-2 border-rad">
+//       <ConfirmLabel
+//         title="Liquidity Provider Fee"
+//         value="1.5 REEF"
+//         titleSize="mini-text"
+//         valueSize="mini-text"
+//       />
+//       <ConfirmLabel
+//         title={`${token1.name} Deposited`}
+//         value={`${token1.amount}`}
+//         titleSize="mini-text"
+//         valueSize="mini-text"
+//       />
+//       <ConfirmLabel
+//         title={`${token2.name} Deposited`}
+//         value={`${token2.amount}`}
+//         titleSize="mini-text"
+//         valueSize="mini-text"
+//       />
+//       <ConfirmLabel
+//         title="Rates"
+//         value={`1 ${token1.name} = ${(
+//           token1.price / token2.price
+//         ).toFixed(8)} ${token2.name}`}
+//         titleSize="mini-text"
+//         valueSize="mini-text"
+//       />
+//       <ConfirmLabel
+//         title=""
+//         value={`1 ${token2.name} = ${(
+//           token2.price / token1.price
+//         ).toFixed(8)} ${token1.name}`}
+//         titleSize="mini-text"
+//         valueSize="mini-text"
+//       />
+//       <ConfirmLabel
+//         title="Share of Pool"
+//         value={`${calculatePoolShare(pool).toFixed(8)} %`}
+//         titleSize="mini-text"
+//         valueSize="mini-text"
+//       />
+//     </div>
+//   </ConfirmationModal>
+// );
 
 const Provide = ({
   state,
@@ -115,17 +114,17 @@ const Provide = ({
   },
 }: Props): JSX.Element => {
   const {
-    settings,
+    // settings,
     token1,
     token2,
     isLoading,
     isValid,
-    newPoolSupply,
-    pool,
+    // newPoolSupply,
+    // pool,
     status,
   } = state;
 
-  const { percentage } = resolveSettings(settings);
+  // const { percentage } = resolveSettings(settings);
   const [perc, setPerc] = useState(0);
 
   const handleSlide = (value: number): void => {
@@ -187,8 +186,10 @@ const Provide = ({
         className="uik-pool-actions__cta-wrapper"
         type="button"
         disabled={!isValid || isLoading}
-        data-bs-toggle="modal"
-        data-bs-target="#supplyModalToggle"
+        // TODO Samo once you have working modal remove on click
+        // data-bs-toggle="modal"
+        // data-bs-target="#supplyModalToggle"
+        onClick={onAddLiquidity}
       >
         <Uik.Button
           className="uik-pool-actions__cta"
@@ -201,14 +202,14 @@ const Provide = ({
         />
       </button>
 
-      <Modal
+      {/* <Modal
         onAddLiquidity={onAddLiquidity}
         newPoolSupply={newPoolSupply}
         token1={token1}
         token2={token2}
         percentage={percentage}
         pool={pool}
-      />
+      /> */}
     </div>
   );
 };
