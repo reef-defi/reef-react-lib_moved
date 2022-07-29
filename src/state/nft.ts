75b1088efee07b80b2127f6b511e98478e498415
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-07-29 19:57:00 +0200

    Nft types fixed

---
+++ src/state/nft.ts
@@ -0,0 +1,21 @@
+import { BasicToken } from "./token";
+
+export type NFTTypes = 'ERC1155' | 'ERC721';
+
+export interface ERC721ContractData {
+  type: 'ERC721'
+  name: string;
+  symbol: string;
+};
+
+export interface ERC1155ContractData {
+  type: 'ERC1155'
+}
+
+export interface NFT extends BasicToken {
+  nftId: number;
+  balance: string;
+  type: NFTTypes;
+  data: ERC1155ContractData | ERC721ContractData;
+}
+
