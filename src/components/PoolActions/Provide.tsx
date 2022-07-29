280e12be00384f3536680ea8411d4e303b887633
Samo Šlag <samo.slag@gmail.com>
2022-07-29 20:06:38 +0200

    Implement new Provide popup

--- src/components/PoolActions/Provide.tsx
+++ src/components/PoolActions/Provide.tsx
@@ -1,14 +1,12 @@
 import { faCoins } from '@fortawesome/free-solid-svg-icons';
 import Uik from '@reef-defi/ui-kit';
 import BigNumber from 'bignumber.js';
-import React, { useMemo } from 'react';
-// import { Pool, TokenWithAmount } from '../../state';
+import React, { useState, useMemo } from 'react';
 import { AddLiquidityState } from '../../store';
-// import { calculatePoolShare } from '../../utils/math';
+import { calculatePoolShare } from '../../utils/math';
 import TokenField from './TokenField';
 
-// import { ConfirmLabel } from '../common/Label';
-// import ConfirmationModal from '../common/Modal';
+import ProvidePopup from './ConfirmPopups/Provide';
 
 export interface ProvideActions {
   onAddLiquidity: () => Promise<void>;
@@ -21,90 +19,6 @@ export interface Props {
   state: AddLiquidityState,
   actions: ProvideActions
 }
-
-// TODO Samo fix modal
-// const Modal = ({
-//   onAddLiquidity,
-//   newPoolSupply,
-//   token1,
-//   token2,
-//   percentage,
-//   pool,
-// }: {
-//   onAddLiquidity: () => Promise<void>,
-//   newPoolSupply: string,
-//   token1: TokenWithAmount,
-//   token2: TokenWithAmount,
-//   percentage: number,
-//   pool?: Pool
-// }): JSX.Element => (
-//   <ConfirmationModal
-//     id="supplyModalToggle"
-//     title="Confirm Supply"
-//     confirmFun={onAddLiquidity}
-//   >
-//     <label className="text-muted ms-2">You will recieve</label>
-
-//     <div className="field border-rad p-3">
-//       <ConfirmLabel
-//         titleSize="h4"
-//         valueSize="h6"
-//         title={newPoolSupply}
-//         value={`${token1.name}/${token2.name}`}
-//       />
-//     </div>
-//     <div className="m-3">
-//       <span className="mini-text text-muted d-inline-block">
-//         Output is estimated. If the price changes by more than
-//         {' '}
-//         {percentage}
-//         % your transaction will revert.
-//       </span>
-//     </div>
-//     <div className="field p-2 border-rad">
-//       <ConfirmLabel
-//         title="Liquidity Provider Fee"
-//         value="1.5 REEF"
-//         titleSize="mini-text"
-//         valueSize="mini-text"
-//       />
-//       <ConfirmLabel
-//         title={`${token1.name} Deposited`}
-//         value={`${token1.amount}`}
-//         titleSize="mini-text"
-//         valueSize="mini-text"
-//       />
-//       <ConfirmLabel
-//         title={`${token2.name} Deposited`}
-//         value={`${token2.amount}`}
-//         titleSize="mini-text"
-//         valueSize="mini-text"
-//       />
-//       <ConfirmLabel
-//         title="Rates"
-//         value={`1 ${token1.name} = ${(
-//           token1.price / token2.price
-//         ).toFixed(8)} ${token2.name}`}
-//         titleSize="mini-text"
-//         valueSize="mini-text"
-//       />
-//       <ConfirmLabel
-//         title=""
-//         value={`1 ${token2.name} = ${(
-//           token2.price / token1.price
-//         ).toFixed(8)} ${token1.name}`}
-//         titleSize="mini-text"
-//         valueSize="mini-text"
-//       />
-//       <ConfirmLabel
-//         title="Share of Pool"
-//         value={`${calculatePoolShare(pool).toFixed(8)} %`}
-//         titleSize="mini-text"
-//         valueSize="mini-text"
-//       />
-//     </div>
-//   </ConfirmationModal>
-// );
 
 const Provide = ({
   state,
@@ -121,8 +35,8 @@ const Provide = ({
     token2,
     isLoading,
     isValid,
-    // newPoolSupply,
-    // pool,
+    newPoolSupply,
+    pool,
     percentage,
     status,
   } = state;
@@ -132,6 +46,8 @@ const Provide = ({
     const sum = firstTokenValue + secondTokenValue;
     return Uik.utils.maxDecimals(sum, 2);
   }, [token1, token2]);
+
+  const [isPopupOpen, setPopupOpen] = useState(false);
 
   return (
     <div>
@@ -177,34 +93,27 @@ const Provide = ({
         />
       </div>
 
-      <button
-        className="uik-pool-actions__cta-wrapper"
-        type="button"
+      <Uik.Button
+        className="uik-pool-actions__cta"
+        fill
+        icon={faCoins}
+        text={isLoading ? status : 'Provide'}
+        size="large"
         disabled={!isValid || isLoading}
-        // TODO Samo once you have working modal remove on click
-        // data-bs-toggle="modal"
-        // data-bs-target="#supplyModalToggle"
-        onClick={onAddLiquidity}
-      >
-        <Uik.Button
-          className="uik-pool-actions__cta"
-          fill
-          icon={faCoins}
-          text={isLoading ? status : 'Provide'}
-          size="large"
-          disabled={!isValid || isLoading}
-          loading={isLoading}
-        />
-      </button>
+        loading={isLoading}
+        onClick={() => setPopupOpen(true)}
+      />
 
-      {/* <Modal
-        onAddLiquidity={onAddLiquidity}
-        newPoolSupply={newPoolSupply}
+      <ProvidePopup
+        isOpen={isPopupOpen}
+        onClose={() => setPopupOpen(false)}
+        onConfirm={onAddLiquidity}
+        poolShare={`${calculatePoolShare(pool).toFixed(8)} %`}
         token1={token1}
         token2={token2}
         percentage={percentage}
-        pool={pool}
-      /> */}
+        LPTokens={newPoolSupply}
+      />
     </div>
   );
 };
