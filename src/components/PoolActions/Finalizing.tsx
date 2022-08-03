50a7f928fd5dc9bf2604a6ea5db321d8d0bf295b
Samo Šlag <samo.slag@gmail.com>
2022-08-03 15:43:53 +0200

    Add finalizing state to Pool Actions

---
+++ src/components/PoolActions/Finalizing.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Uik from '@reef-defi/ui-kit';
+import './finalizing.css';
+
+const Finalizing = (): JSX.Element => (
+  <div className="pool-actions-finalizing">
+    <div className="pool-actions-finalizing__animation">
+      <Uik.FishAnimation />
+      <Uik.FishAnimation />
+    </div>
+    <Uik.Text text="We're just finalizing your transaction." />
+    <Uik.Text type="mini" text="This proccess usually takes up to one minute." />
+  </div>
+);
+
+export default Finalizing;
