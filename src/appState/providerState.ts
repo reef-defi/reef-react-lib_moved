e8deeb6c23d6cc518a8dec3655d91c2caeaa8bb6
Anukul Pandey <anukul.030601@gmail.com>
2023-08-24 00:47:16 +0530

    fix: return type of currentNetwork

--- src/appState/providerState.ts
+++ src/appState/providerState.ts
@@ -1,23 +1,14 @@
-import { ReplaySubject, shareReplay } from 'rxjs';
+import { Observable, ReplaySubject, shareReplay } from 'rxjs';
 import { Provider } from '@reef-defi/evm-provider';
+import {
+  reefState,
+} from '@reef-chain/util-lib';
 import { Network } from '../state';
 
 export const providerSubj: ReplaySubject<Provider> = new ReplaySubject<Provider>(1);
-export const selectedNetworkSubj: ReplaySubject<Network> = new ReplaySubject<Network>();
 
 export const ACTIVE_NETWORK_LS_KEY = 'reef-app-active-network';
 export const currentProvider$ = providerSubj.asObservable().pipe(shareReplay(1));
 export const setCurrentProvider = (provider: Provider): void => providerSubj.next(provider);
-
-export const currentNetwork$ = selectedNetworkSubj.asObservable();
-export const setCurrentNetwork = (network: Network): void => {
-  if (network != null) {
-    try {
-      localStorage.setItem(ACTIVE_NETWORK_LS_KEY, JSON.stringify(network));
-    } catch (e) {
-      // when cookies disabled localStorage can throw
-    }
-  }
-  selectedNetworkSubj.next(network);
-};
-currentNetwork$.subscribe((network) => console.log('SELECTED NETWORK=', network));
+export const currentNetwork$ = reefState.selectedNetwork$ as Observable<Network>;
+export const setCurrentNetwork = reefState.setSelectedNetwork;
