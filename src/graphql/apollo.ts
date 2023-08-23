86aa897ddb6cd95adf89e5c7d458c864f11b4cce
Anukul Pandey <anukul.030601@gmail.com>
2023-08-23 14:33:36 +0530

    feat: removed unused code

--- src/graphql/apollo.ts
+++ src/graphql/apollo.ts
@@ -68,42 +68,4 @@ export const setApolloDexUrls = (urls: GQLUrl): void => {
   apolloDexUrlsSubj.next(urls);
 };
 
-const splitDexLink$ = apolloDexUrlsSubj.pipe(
-  map((urls: GQLUrl) => {
-    const httpLink = new HttpLink({
-      uri: urls.http,
-    });
-    const wsLink = new GraphQLWsLink(createClient({
-      url: urls.ws,
-    }));
-    return split(
-      ({ query }) => {
-        const definition = getMainDefinition(query);
-
-        return (
-          definition.kind === 'OperationDefinition'
-                    && definition.operation === 'subscription'
-        );
-      },
-      wsLink,
-      httpLink,
-    );
-  }),
-  shareReplay(1),
-);
-const apolloDexLinksClientInstance$: Observable<ApolloClient<any>> = splitDexLink$.pipe(
-  map(
-    (splitLink) => new ApolloClient({
-      cache: new InMemoryCache(),
-      link: ApolloLink.from([splitLink]),
-    }),
-  ),
-  shareReplay(1),
-);
-
-export const apolloDexClientInstance$: Observable<ApolloClient<any>> = merge(apolloDexLinksClientInstance$, apolloDexClientSubj).pipe(
-  distinctUntilChanged(),
-  shareReplay(1),
-);
-
 export const zenToRx = <T>(zenObservable: ZenObservable<T>): Observable<T> => new Observable((observer) => zenObservable.subscribe(observer));
